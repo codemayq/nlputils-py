88758b2e2fec40ab1d169b3f2bcce4390982ea89
mayongqiang <mayongqiang@corp.netease.com>
2021-12-04 15:16:47 +0800

    rename

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
     url=URL,
     # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
-    py_modules=['lang_util'],
+    py_modules=['nlputils'],
 
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
